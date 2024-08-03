68f85b8f93f282fb7c12011db84becf88ece2720
mohitgoyat525 <montyits71@gmail.com>
2024-08-03 21:50:27 +0530

    add-nav-bar

---
+++ assets/js/app.js
@@ -0,0 +1,7 @@
+const navmenu = () => {
+  document.getElementById("navlinks").classList.toggle("show-nav");
+  document.body.classList.toggle("overflow-hidden");
+  document.getElementById("rotated-1").classList.toggle("rotated-1");
+  document.getElementById("rotated-2").classList.toggle("rotated-2");
+  document.getElementById("transparent").classList.toggle("bg-transparent");
+};
