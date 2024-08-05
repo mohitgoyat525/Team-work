b47a8ec45211612c5c1c0049a45aff0f749e377e
mohitgoyat525 <montyits71@gmail.com>
2024-08-05 16:23:42 +0530

    add-slick-slider-links

--- assets/js/app.js
+++ assets/js/app.js
@@ -5,3 +5,41 @@ const navmenu = () => {
   document.getElementById("rotated-2").classList.toggle("rotated-2");
   document.getElementById("transparent").classList.toggle("bg-transparent");
 };
+
+
+
+// $('slick-slider-box').slick({
+//   dots: false,
+//   infinite: true,
+//   speed: 400,
+//   slidesToShow: 3.7,
+//   slidesToScroll: 1,
+//   autoplay: true,
+//   autoplaySpeed: 2000,
+
+//   responsive: [
+//     {
+//       breakpoint: 1200,
+//       settings: {
+//         slidesToShow: 3,
+//         slidesToScroll: 1,
+//         infinite: true,
+//         dots: true,
+//       },
+//     },
+//     {
+//       breakpoint: 982,
+//       settings: {
+//         slidesToShow: 2,
+//         slidesToScroll: 1,
+//       },
+//     },
+//     {
+//       breakpoint: 568,
+//       settings: {
+//         slidesToShow: 1,
+//         slidesToScroll: 1,
+//       },
+//     },
+//   ],
+// });
